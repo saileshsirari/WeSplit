6f01a1901347330d6d0335fed88130f6ef1b802c
saileshsirari <sailesh.sirari@gmail.com>
2024-10-02 17:31:04 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by sailesh sirari on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
